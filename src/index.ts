06eb1553f983b9cce99cbc16e8405742daffd12e
Iikka Hauhio <iikka@kaivos.org>
2019-01-18 12:53:35 +0200

    wip: websockets + localization

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
 import express from 'express';
+import * as WebSocket from 'ws';
+import Localize = require('localize');
+
+const localize = new Localize('./i18n/');
+localize.setLocale('fi');
+const _: (str: string, ...subs: any[]) => string = localize.translate;
+
+const wss_port = parseInt(process.env.WSS_PORT || '3030', 10);
+const wss = new WebSocket.Server({ port: wss_port });
+
+wss.on('connection', function connection(ws) {
+	ws.on('message', function incoming(message) {
+		console.log('received: %s', message);
+	});
+
+	console.log(_('New connection'));
+});
 
 const app = express();
 
 // Fallback to index.html
 app.get('/', (_req, res) => {
-	return res.json({message: 'Hello World!'})
+	return res.json({ message: 'Hello World!' })
 });
 
 const port = parseInt(process.env.PORT || '3000', 10);
 app.listen(port, () => {
-	console.log(`Server listening on port ${port}`);
+	console.log(_("Server listening on port $[1]", port));
 });
