16a364822c9d898fcb0fc43a1f0c04ee64ccbe90
Olavi Mustanoja <tilastokeskus@gmail.com>
2019-01-18 11:52:03 +0200

    chore: add tslint

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ const app = express();
 
 // Fallback to index.html
 app.get('/', (_req, res) => {
-	return res.json({message: 'Hello World!'})
+	return res.json({message: 'Hello World!'});
 });
 
 const port = parseInt(process.env.PORT || '3000', 10);
 app.listen(port, () => {
-	console.log(`Server listening on port ${port}`);
+	console.log(`Server listening on port ${port}`); // tslint:disable-line:no-console
 });
