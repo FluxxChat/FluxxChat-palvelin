e483051d63ac4e66fa35dda02d07c7fb5aeadfb5
Iikka Hauhio <iikka@kaivos.org>
2019-02-01 15:29:47 +0200

    refactor: name, description and parameterTypes of MuteRule

--- src/rules/mute-rule.ts
+++ src/rules/mute-rule.ts
@@ -1,10 +1,14 @@
 import {Rule, RuleCategory, RuleBase} from './rule';
 import {FluxxChatServer} from '../server';
-import {Message} from 'fluxxchat-protokolla';
+import {Message, RuleParameterTypes} from 'fluxxchat-protokolla';
 import {Connection} from '../connection';
 
 export class MuteRule extends RuleBase implements Rule {
 	public ruleCategories: Set<RuleCategory> = new Set(['MUTE'] as RuleCategory[]);
+	public title = global._('Mute Player');
+	public description = global._('Mutes a specific player.');
+	public ruleName = 'mute';
+	public parameterTypes = {target: 'player'} as RuleParameterTypes;
 
 	public applyMessage(_server: FluxxChatServer, message: Message, parameter: any, sender: Connection): Message | null {
 		if (sender.id === parameter.target) {
