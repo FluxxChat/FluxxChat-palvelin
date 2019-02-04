eeb96371a5e487ef2a463b7818e312618c0ef20d
Onni Aarne <onni.aarne@gmail.com>
2019-02-04 15:15:29 +0200

    feat: cards for enabling and disabling markdown

--- src/rules/active-rules.ts
+++ src/rules/active-rules.ts
@@ -2,10 +2,12 @@ import {DisablingRule} from './rule';
 import {AnonymityRule} from './anonymity-rule';
 import {MessageLengthRule} from './message-length-rule';
 import {MuteRule} from './mute-rule';
+import {MarkdownRule} from './formatting-rule';
 
 const ANONYMITY = new AnonymityRule();
 const MESSAGE_LENGTH = new MessageLengthRule();
 const MUTE = new MuteRule();
+const MARKDOWN = new MarkdownRule();
 
 export const RULES = {
 	anonymity: ANONYMITY,
@@ -14,5 +16,7 @@ export const RULES = {
 	no_message_length: new DisablingRule([MESSAGE_LENGTH], 'no_message_length'),
 	mute: MUTE,
 	unmute_all: new DisablingRule([MUTE], 'unmute_all'),
+	markdown_formatting: MARKDOWN,
+	disable_formatting: new DisablingRule([MARKDOWN], 'disable_formatting'),
 	disable_all: new DisablingRule([ANONYMITY, MESSAGE_LENGTH, MUTE], 'disable_all')
 };
