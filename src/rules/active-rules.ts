6410384170da986959e453d513895d2333898ec1
Iikka Hauhio <iikka@kaivos.org>
2019-02-15 12:37:49 +0200

    refactor: generalize verb limit to all parts of speech

--- src/rules/active-rules.ts
+++ src/rules/active-rules.ts
@@ -4,22 +4,24 @@ import {MessageLengthRule} from './message-length-rule';
 import {MuteRule} from './mute-rule';
 import {MarkdownRule} from './formatting-rule';
 import {PseudonymeRule} from './pseudonyme-rule';
-import {VerbLimitRule} from './verb-limit-rule';
+import {PosMaxLimitRule, PosMinLimitRule} from './verb-limit-rule';
 
 const ANONYMITY = new AnonymityRule();
 const MESSAGE_LENGTH = new MessageLengthRule();
 const MUTE = new MuteRule();
 const MARKDOWN = new MarkdownRule();
 const PSEUDONYMES = new PseudonymeRule();
-const VERB_LIMIT = new VerbLimitRule();
+const POS_MAX_LIMIT = new PosMaxLimitRule();
+const POS_MIN_LIMIT = new PosMinLimitRule();
 
 export const RULES: {[key: string]: Rule} = {
 	anonymity: ANONYMITY,
 	no_anonymity: new DisablingRule([ANONYMITY], 'no_anonymity'),
 	message_length: MESSAGE_LENGTH,
 	no_message_length: new DisablingRule([MESSAGE_LENGTH], 'no_message_length'),
-	verb_limit: VERB_LIMIT,
-	no_verb_limit: new DisablingRule([VERB_LIMIT], 'no_verb_limit'),
+	pos_max_limit: POS_MAX_LIMIT,
+	pos_min_limit: POS_MIN_LIMIT,
+	no_pos_limit: new DisablingRule([POS_MAX_LIMIT, POS_MIN_LIMIT], 'no_pos_limit'),
 	mute: MUTE,
 	unmute_all: new DisablingRule([MUTE], 'unmute_all'),
 	markdown_formatting: MARKDOWN,
--- src/rules/verb-limit-rule.ts
+++ src/rules/verb-limit-rule.ts
@@ -1,39 +1,60 @@
 import {Rule, RuleCategory, RuleBase} from './rule';
 import {FluxxChatServer} from '../server';
-import {TextMessage, RuleParameterTypes} from 'fluxxchat-protokolla';
+import {TextMessage, RuleParameterTypes, RuleParameters} from 'fluxxchat-protokolla';
 import {Connection} from '../connection';
 import libvoikko from '../../lib/libvoikko';
 
 const Libvoikko = libvoikko();
 
-export class VerbLimitRule extends RuleBase implements Rule {
-	public ruleCategories = new Set([RuleCategory.VERB_NUMBER]);
-	public title = global._('Minumum Verb Number');
-	public description = global._('Sets the minumum number of verbs in message.');
-	public ruleName = 'verb_limit';
-	public parameterTypes = {number: 'number'} as RuleParameterTypes;
+const VOIKKO_POS_IDS = {
+	verb: ['teonsana'],
+	noun: ['nimisana'],
+	adjective: ['laatusana', 'nimisana_laatusana']
+};
+
+class BasePosLimitRule extends RuleBase {
+	public ruleCategories = new Set([RuleCategory.POS_LIMIT]);
 
 	private voikko?: libvoikko.Voikko;
 
-	public isValidMessage(_server: FluxxChatServer, message: TextMessage, parameter: any, _sender: Connection) {
-		return this.getNumberOfVerbs(message.textContent) >= parameter.number;
-	}
-
-	private getNumberOfVerbs(message: string): number {
+	protected getNumberOfWordsWithPos(message: string, pos: keyof typeof VOIKKO_POS_IDS): number {
 		if (!this.voikko) {
 			this.voikko = Libvoikko.init('fi');
 		}
+		const voikkopos = VOIKKO_POS_IDS[pos];
 		const tokens = this.voikko.tokens(message);
 		let verbs = 0;
 		for (const token of tokens) {
 			if (token.type === 'WORD') {
 				const a = this.voikko.analyze(token.text);
-				const isVerb = a.some(info => info.CLASS === 'teonsana');
+				const isVerb = a.some(info => voikkopos.includes(info.CLASS));
 				if (isVerb) {
 					verbs++;
 				}
 			}
 		}
 		return verbs;
+	}
+}
+
+export class PosMinLimitRule extends BasePosLimitRule implements Rule {
+	public title = global._('Minumum Number of Verbs/Nouns/Adjectives');
+	public description = global._('Sets the minumum number of verbs/nouns/adjectives in message.');
+	public ruleName = 'pos_min_limit';
+	public parameterTypes = {pos: Object.keys(VOIKKO_POS_IDS), number: 'number'} as RuleParameterTypes;
+
+	public isValidMessage(_server: FluxxChatServer, message: TextMessage, parameter: RuleParameters, _sender: Connection) {
+		return this.getNumberOfWordsWithPos(message.textContent, parameter.pos) >= parameter.number;
+	}
+}
+
+export class PosMaxLimitRule extends BasePosLimitRule implements Rule {
+	public title = global._('Maximum Number of Verbs/Nouns/Adjectives');
+	public description = global._('Sets the maximum number of verbs/nouns/adjectives in message.');
+	public ruleName = 'pos_max_limit';
+	public parameterTypes = {pos: Object.keys(VOIKKO_POS_IDS), number: 'number'} as RuleParameterTypes;
+
+	public isValidMessage(_server: FluxxChatServer, message: TextMessage, parameter: RuleParameters, _sender: Connection) {
+		return this.getNumberOfWordsWithPos(message.textContent, parameter.pos) <= parameter.number;
 	}
 }
