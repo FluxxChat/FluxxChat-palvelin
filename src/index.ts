34f0012074786bd7324a32e4ed50f9806750eab0
Olavi Mustanoja <tilastokeskus@gmail.com>
2019-04-17 13:57:44 +0300

    feat: admin endpoint

--- src/index.ts
+++ src/index.ts
@@ -18,12 +18,17 @@
 import path from 'path';
 import http from 'http';
 import express from 'express';
+import uuid from 'uuid';
 import * as WebSocket from 'ws';
-import {basicAuth} from './lib/auth-middleware';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import {basicAuth, tokenAuth} from './lib/auth-middleware';
 import * as events from './event-models';
 
 import {FluxxChatServer} from './server';
 import {Connection} from './connection';
+
+const TOKEN_EXPIRE_MINUTES = 120;
 
 events.setFilePath(path.resolve(__dirname, '../events.log'));
 const server = new FluxxChatServer();
@@ -36,26 +41,61 @@ wss.on('connection', function connection(ws: WebSocket) {
 	server.addConnection(new Connection(ws));
 });
 
-// User-pass map
+// User:pass map
 const users = new Map();
+
+// access-token:token-data map
+const tokens = new Map();
 
 // Add a user for local development
 if (!process.env.NODE_ENV) {
 	users.set('admin', 'admin');
 }
 
-// Populate user-pass map from environment
-const logins = (process.env.LOGINS || '').split(';').map(l => l.split(':'));
-for (const [user, pass] of logins) {
-	users.set(user, pass);
+if (process.env.LOGINS) {
+	// Populate user-pass map from environment
+	const logins = process.env.LOGINS.split(';').map(l => l.split(':'));
+	for (const [user, pass] of logins) {
+		users.set(user, pass);
+	}
 }
 
 (async () => {
-	await events.initDb();
+	const db = await events.initDb();
 
-	// Event logs endpoint with http basic auth
-	app.get('/events', basicAuth(users), (_req, res) => {
-		return res.sendFile(path.resolve(__dirname, '../events.log'));
+	app.use('/admin', cors());
+
+	app.post('/admin/token', basicAuth(users), (_req, res) => {
+		const expireMillis = 1000 * 60 * TOKEN_EXPIRE_MINUTES;
+
+		const tokenData = {
+			accessToken: uuid.v4(),
+			exp: Date.now() + expireMillis
+		};
+
+		tokens.set(tokenData.accessToken, tokenData);
+
+		setTimeout(() => {
+			tokens.delete(tokenData.accessToken);
+		}, expireMillis);
+
+		res.json({data: tokenData});
+	});
+
+	app.post('/admin/query', [tokenAuth(tokens), bodyParser.json()], async (req, res) => {
+		const query = req.body.query;
+
+		// Make sure query is a select statement
+		if (!/^select/i.test(query)) {
+			return res.status(400).json({error: {status: 400, message: 'Only select statements are supported'}});
+		}
+
+		try {
+			const response = await db.raw(query);
+			res.json({data: response});
+		} catch (err) {
+			res.status(500).json({error: {status: 500, message: err.message}});
+		}
 	});
 
 	// Fallback to index.html
--- src/lib/auth-middleware.ts
+++ src/lib/auth-middleware.ts
@@ -7,9 +7,28 @@ export function basicAuth(users: Map<string, string>) {
 
 		if (!validLogin) {
 			res.set('WWW-Authenticate', 'Basic realm="401"');
-			res.status(401).send('Authentication required');
-			return;
+			return res.status(401).json({error: {status: 401, message: 'Authentication required'}});
 		}
+
+		next();
+	};
+}
+
+export function tokenAuth(tokens: Map<string, {exp: number}>) {
+	return (req, res, next) => {
+		const token = (req.headers.authorization || '').split(' ')[1] || '';
+
+		const tokenData = tokens.get(token);
+		const tokenExpired = tokenData && tokenData.exp < Date.now();
+
+		if (tokenExpired) {
+			tokens.delete(token);
+		}
+
+		if (!tokenData || tokenExpired) {
+			return res.status(401).json({error: {status: 401, message: 'Invalid access token'}});
+		}
+
 		next();
 	};
 }
